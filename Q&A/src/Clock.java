2abf1fec97e1610ad3ba08ca9f63da700ea7faf4
jason <jason@jasons-mbp-2.fios-router.home>
2020-10-01 21:25:53 -0400

    Clock

---
+++ Q&A/src/Clock.java
@@ -0,0 +1,48 @@
+public class Clock {
+	int tick;
+	int time;
+
+	public void setTime(int x) {
+	time=x; 
+	}
+	
+	public void tick() {
+		time=time+1;
+
+	
+	}
+	
+	
+	public void displayTime(){
+		
+		System.out.println(time);
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Clock tester = new Clock();
+		
+		// should display the time to be 56, 57, 58, 59, 0, 1, etc.
+		tester.setTime(56);
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		
+		
+		
+		
+	}
+}
