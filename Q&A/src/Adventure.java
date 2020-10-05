7ff4b6e4ebfd2d6220fb498e22fd85c3aa2cad46
jason <jason@10.1.99.6>
2020-10-05 14:54:13 -0400

    Project

---
+++ Q&A/src/Adventure.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Adventure {
+//Jason's adventure project
+
+
+Scanner input = new Scanner(System.in);
+
+public void Ask4YourName() {
+
+System.out.print("Welcome, what's your name?");
+
+String name = input.next();
+
+System.out.println("How could I forget, you're" + name + "");
+
+}
+	
+	
+}
